a365c1d269fbe823597399bd611e96ce764724f9
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-06-08 17:53:18 +0900

    feat(Fragment): add FragmentLifecycle study project

---
+++ android/books/android-kotlin-build/FragmentLifecycle/app/src/main/java/com/example/fragmentlifecycle/MainFragment.kt
@@ -0,0 +1,81 @@
+package com.example.fragmentlifecycle
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentlifecycle.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private val param1: String? by lazy { arguments?.getString(ARG_PARAM1) }
+    private val param2: String? by lazy { arguments?.getString(ARG_PARAM2) }
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated")
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.d(TAG, "onDestroyView")
+        _binding = null
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.d(TAG, "onDetach")
+        super.onDetach()
+    }
+
+    companion object {
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+        private const val TAG = "MainFragment"
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = MainFragment().apply {
+            arguments = Bundle().apply {
+                putString(ARG_PARAM1, param1)
+                putString(ARG_PARAM2, param2)
+            }
+        }
+    }
+}
